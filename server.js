e64071b6d5aa741a19c0c5e029bd0a424228edc2
Blake Burns <bburns@mysgi.com>
2022-11-20 20:41:53 -0500

    added seeds.sql content to populate schema tables, updated server.js content

--- server.js
+++ server.js
@@ -1,13 +1,6 @@
-const fs = require('fs');
-const express = require('express');
 const mysql = require('mysql2');
+const cTable = require('console.table');
 const inquirer = require('inquirer');
-
-const PORT = process.env.PORT || 3001;
-const app = express();
-
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
 
 const db = mysql.createConnection(
   {
