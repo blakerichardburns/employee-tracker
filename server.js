8f89d4830c494981c196882bb6c16efc1e012a0b
Blake Burns <bburns@mysgi.com>
2022-11-22 16:38:10 -0500

    got addRole function working

--- server.js
+++ server.js
@@ -31,18 +31,6 @@ const addDepartment = [
   }
 ];
 
-const addRole = [
-  {
-    name: 'roleName',
-    type: 'input',
-    message: 'What is the name of the Role would you like to add?',
-  },
-  {
-    name: 'salary',
-    type: 'input',
-    message: 'What is the salary of the role?',
-  }
-];
 
 const addEmployee = [
   {
@@ -60,26 +48,26 @@ const addEmployee = [
 
 function init() {
   inquirer
-    .prompt(mainMenu)
-    .then(response => {
-      if (response.mainMenu === 'View all departments') {
-        viewDepartments();
-      } else if (response.mainMenu === 'View all roles') {
-        viewRoles();
-      } else if (response.mainMenu === 'View all employees') {
-        viewEmployees();
-      } else if (response.mainMenu === 'Add a department') {
-        createDepartment();
-      } else if (response.mainMenu === 'Add a role') {
-        createRole();
-      } else if (response.mainMenu === 'Add an employee') {
-        createEmployee();
-      } else if (response.mainMenu === 'Update an employee role') {
-        updateRole();
-      } else {
-        process.exit();
-      }
-    })
+  .prompt(mainMenu)
+  .then(response => {
+    if (response.mainMenu === 'View all departments') {
+      viewDepartments();
+    } else if (response.mainMenu === 'View all roles') {
+      viewRoles();
+    } else if (response.mainMenu === 'View all employees') {
+      viewEmployees();
+    } else if (response.mainMenu === 'Add a department') {
+      createDepartment();
+    } else if (response.mainMenu === 'Add a role') {
+      createRole();
+    } else if (response.mainMenu === 'Add an employee') {
+      createEmployee();
+    } else if (response.mainMenu === 'Update an employee role') {
+      updateRole();
+    } else {
+      process.exit();
+    }
+  })
 };
 
 init();
@@ -113,33 +101,72 @@ function viewEmployees() {
 
 function createDepartment() {
   inquirer
-    .prompt(addDepartment)
-      .then(response => {
-        db.query(`INSERT INTO department (name) VALUES ("${response.addDepartment}");`);
-        console.log(`${response.addDepartment} department successfully added!`);
-        viewDepartments();
-        init();
-      });
-      
+  .prompt(addDepartment)
+  .then(response => {
+    db.query(`INSERT INTO department (name) VALUES ("${response.addDepartment}");`);
+    db.query("SELECT * FROM department", (err, results) => {
+      if (err) {
+        console.log("Error");
+      } console.table(results);
+      console.log(`${response.addDepartment} department successfully added!`);
+      init();
+    });
+  });
+  
 };
 
-// function createRole() {
-//   db.query("SELECT * FROM department", (err, results) => {
-//     if (err) {
-//       console.log("Error");
-//     } let departments = results.map(({ id, name }) => ({ value: id, name: name }))
-//   });
-//   inquirer
-//     .prompt(addRole)
-//       .then(response => {
-//         db.query(`INSERT INTO role (title, salary) VALUES (${response.roleName}, ${response.salary})`)
-//       })
-// };
+function createRole() {
+
+  db.query("SELECT * FROM department", (err, results) => {
+    if (err) {
+      console.log("Error");
+    };
+    let departments = results.map(({ id, name }) => ({ value: id, name: `${id} ${name}` }));
+    let departmentsArray = [];
+
+    for (let i = 0; i < departments.length; i++) {
+      departmentsArray.push(departments[i].name);
+    };
+      var addRole = [
+        {
+          name: 'roleName',
+          type: 'input',
+          message: 'What is the name of the Role would you like to add?',
+        },
+        {
+          name: 'roleSalary',
+          type: 'input',
+        message: 'What is the salary of the role?',
+      },
+      {
+        name: 'roleDepartment',
+        type: 'list',
+        message: 'In which department does the role belong?',
+        choices: departmentsArray
+      }
+    ];
+
+    inquirer
+    .prompt(addRole)
+    .then(response => {
+        let departmendIdString = response.roleDepartment
+        let departmentIdArray = departmendIdString.split(" ");
+      db.query(`INSERT INTO role (title, salary, department_id) VALUES ("${response.roleName}", "${response.roleSalary}", "${departmentIdArray[0]}");`);
+      db.query("SELECT * FROM role", (err, results) => {
+        if (err) {
+          console.log("Error");
+        } console.table(results);
+        console.log(`${response.roleName} role successfully added!`);
+        init();
+        });
+    });
+  });
+};
 
 // function createEmployee() {
-
-// };
-
-// function updateRole() {
-
-// };
+  
+//   };
+  
+//   function updateRole() {
+    
+//     };
